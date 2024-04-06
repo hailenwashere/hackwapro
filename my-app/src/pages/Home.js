4437f2dd1353867ea2af64765e2a7485abed3a71
Jacky Dai <ykcaj@g.ucla.edu>
2024-04-06 14:17:05 -0700

    links+

---
+++ my-app/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React, {useEffect, useState} from "react";
+// import { useNavigate } from "react-router";
+
+import '../styles/Home.css';
+
+const Home = () => {
+    return (
+        <body>
+        <div>
+        <div className="title">Home</div>
+        </div>
+        </body>
+        
+    )
+}
+
+export default Home;
