355d4446136b632bf9351602f277b06656d39282
Yifan Tang <yifant@g.ucla.edu>
2024-04-06 16:14:16 -0700

    Created Add Ingredient Component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,42 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import Login from "./pages/Login.js";
-import Landing from "./pages/Landing.js";
-import Home from "./pages/Home.js";
+import Login from './pages/Login.js';
+import Landing from './pages/Landing.js';
+import Home from './pages/Home.js';
 import reportWebVitals from './reportWebVitals';
 import IngredientRequest from './pages/IngredientRequest.js';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import AddIngredient from './pages/AddIngredient.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-  <React.StrictMode>
-      <Router>
-     <Routes>
-       <Route exact path="/" element={<Landing/>}/>
-       <Route exact path="/login" element={<Login/>}/>
-       <Route exact path="/home" element={<Home/>}/>
-       <Route exact path="/request" element={<IngredientRequest/>}/>
-     </Routes>
-     </Router>
-  </React.StrictMode>
-  <a href="./"> land</a>
-  <a href="./login"> login</a>
-  <a href="./home"> home</a>
-  </div>
+    <div>
+        <React.StrictMode>
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<Landing />} />
+                    <Route exact path="/login" element={<Login />} />
+                    <Route exact path="/home" element={<Home />} />
+                    <Route exact path="/add" element={<AddIngredient />} />
+                    <Route exact path="/request" element={<IngredientRequest />} />
+                </Routes>
+            </Router>
+        </React.StrictMode>
+        <a href="./"> land</a>
+        <a href="./login"> login</a>
+        <a href="./home"> home</a>
+    </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
 
 // // import React from 'react';
 // import ReactDOM from 'react-dom';
@@ -59,7 +55,7 @@ reportWebVitals();
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
 //   <div>
-    
+
 //     <React.StrictMode>
 //     <Router>
 //     <Routes>
@@ -79,9 +75,9 @@ reportWebVitals();
 //   <a href="./signup"> signup</a>
 //   <a>   </a>
 //   <a href="./login"> login.</a>
-  
+
 //   </div>
-  
+
 // );
 
 // // If you want to start measuring performance in your app, pass a function
