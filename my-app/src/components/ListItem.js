222af611da4d2c75ddde7ccce6590749d771337c
Helen Feng <53279829+hailenwashere@users.noreply.github.com>
2024-04-06 18:28:59 -0700

    fix frontend to support new schema

--- my-app/src/components/ListItem.js
+++ my-app/src/components/ListItem.js
@@ -2,18 +2,39 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom'
 
 export default function ListItem({ props }) {
+    /* props looks like this 
+        [
+            "chicken",
+            {
+                "total_quantity": 4,
+                "min_expiry": "01/24/2024",
+                "ingredient_info": {
+                    "Yifan": {
+                        "quantity": 2,
+                        "expiration": "01/24/2024"
+                    },
+                    "Ellen": {
+                        "quantity": 2,
+                        "expiration": "01/30/2024"
+                    }
+                }
+            },
+        ]
+    */
+
     const navigate = useNavigate()
     const onClick = () =>
     {
-        localStorage.setItem("ingredientinfo", JSON.stringify(props))
+        localStorage.setItem("currentIngredient", props[0])
+        localStorage.setItem("ingredientSummary", JSON.stringify(props[1]))
         navigate('/request')
     }
 
     return (
         <tr className="listItem">
-        <td>{props.ingredient_name}</td>
-        <td>{props.total_quantity}</td>
-        <td>{props.min_expiry}</td>
+        <td>{props[0]}</td>
+        <td>{props[1].total_quantity}</td>
+        <td>{props[1].min_expiry}</td>
         <td>
             <button onClick={ onClick }>Request</button>
         </td>
