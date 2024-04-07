7a8e5244719aafe0c7015a0d3526b809c3b3c4b7
Jacky Dai <ykcaj@g.ucla.edu>
2024-04-06 20:13:10 -0700

    added header links

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -2,9 +2,20 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
 import '../styles/Header.css';
+import { useNavigate } from 'react-router';
 
 
 export default function Header() {
+  const navigate = useNavigate();
+  const gotoAdd = () => {
+    // Define the logic for navigating to the desired location
+    navigate("/add");
+  };
+  const gotoGPT = () => {
+    // Define the logic for navigating to the desired location
+    navigate("/gpt");
+  };
+
     return (
         <header className="header">
           <div className="left">
@@ -13,8 +24,8 @@ export default function Header() {
           <div className="right">
             <button className="button_header button-short">my items</button>
             <button className="button_header button-short">summary</button>
-            <button className="button_header button-short">generate recipe</button>
-            <button className="button_header add_item"><FontAwesomeIcon icon={faCirclePlus} /> ADD ITEM</button>
+            <button className="button_header button-short" onClick={gotoGPT} >generate recipe</button>
+            <button className="button_header add_item"onClick={gotoAdd}><FontAwesomeIcon icon={faCirclePlus} /> ADD ITEM</button>
           </div>
         </header>
       );
