c7d351b15bfff8595570dd61e0304dd3e673066f
Jacky Dai <ykcaj@g.ucla.edu>
2024-04-06 19:37:09 -0700

    header added from ellen

---
+++ my-app/src/components/Header_centered.js
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+import '../styles/Header_centered.css';
+
+export default function Header() {
+    return (
+        <header className="header">
+        <h1 className="title">OURFRIDGE</h1>
+      </header>
+      );
+}
